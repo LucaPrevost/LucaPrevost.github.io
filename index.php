79ee7fd89b3a0f5b71ad99c7725d89fe9d06abe2
LucaPrevost <prevost.luca@gmail.com>
2022-09-26 10:36:24 +0200

    Resa jeux boussole

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La Boussole</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+
+
+<body>
+
+
+<nav>
+    <div class="association">
+
+        <div>
+            <img class="logo_boussole" src="Images/Logo sans écriture.png" alt="logo_boussole">
+        </div>    
+        
+        <div>
+            <h1>La Boussole</h1>
+            <h2>Café jeux</h2>
+        </div>
+
+
+    </div>
+
+
+    <div class="user">
+    
+        <div>
+            <img class="meeple_user" src="Images/meeple_user.png" alt="meeple_user">
+        </div>
+        <div> 
+            <h3>Mon compte</h3>
+        </div>
+
+    </div>
+
+</nav>
+
+<!-- =============================================== ESPACE CENTRALE DE LA PAGE ===================================================================================== --> 
+
+<main>
+
+    <div class=nexttime>
+
+        <h1>Soirée du 23 Septembre 2022</h1>
+        <h2>Tables de jeux</h2>
+
+    </div>
+
+    <div class=listetable>
+
+
+    </div>
+
+
+
+</main>
+
+<footer>
+
+    <div>
+        <img class = add src="Images/add.png" alt="ajout">
+    </div>
+
+</footer>
+
+
+</body>
+</html>
